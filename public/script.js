3ce719c6657da5ca86c195bffd29b4289741124f
WAGNER Omar Ben Mathieu <mathieu@MacBook-Air-de-WAGNER.local>
2026-02-05 23:45:53 +0100

    Update

--- public/script.js
+++ public/script.js
@@ -427,7 +427,7 @@ function renderCalendarPage(pageName) {
         </div>
       </div>
 
-      <div class="calendar-grid ${page === "buy" ? "buy-calendar" : ""}">
+      <div class="calendar-grid">
         ${dows.map((d) => `<div class="dow">${d}</div>`).join("")}
 
         ${cells
