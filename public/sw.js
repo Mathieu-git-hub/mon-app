643c51bee7d17cb02d08ba9504c81933095942cc
WAGNER Omar Ben Mathieu <mathieu@MacBook-Air-de-WAGNER.local>
2026-02-10 20:25:44 +0100

    Update

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "monapp-v1";
+
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/script.js",
+  "/manifest.webmanifest",
+  "/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
